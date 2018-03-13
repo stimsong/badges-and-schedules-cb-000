08138d2c9882775028b017ff58d1072e5e032abc
Greg Stimson <stimsong@yahoo.com>
2018-03-13 19:57:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -34,9 +34,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-#  batch_badge_creator(attendees)
+  batch_badge_creator(attendees)
 
-  batch_badge_creator(attendees).each do |badge|
+  attendees_badges.each do |badge|
     puts "#{badge}"
   end
 
