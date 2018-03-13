9b72db898f4f095ca5a24b8a263931ec7aaa2783
Greg Stimson <stimsong@yahoo.com>
2018-03-13 19:43:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees_badges = []
+  count = 0
+  while count <= attendees.length-1
+    attendees_badges << "Hello, my name is #{attendees[count]}."
+    count += 1
+  end
+  attendees_badges
+end
+
+def assign_rooms(attendees)
+#  room_assign_from_index = []
+  attend_room_assign = []
+
+#
+#  started to use each_with_index but it seemed more complicated
+#  than using a simple while loop.
+#
+#  attendees.each_with_index { |item, index|
+#    room_assign_from_index[item] = index + 1
+#  }
+  count = 0
+  while count <= attendees.length-1
+    attend_room_assign << "Hello, #{attendees[count]}! You'll be assigned to room #{count +1 }!"
+    count += 1
+  end
+  attend_room_assign
+end
+
+def printer(attendees)
+  batch_badge_creator
+
+  attendees_badges.each do |badge|
+    puts "#{badge}"
+  end
+
+end
